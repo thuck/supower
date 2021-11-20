b45b3ed398ac4dd567fcf803879e25437fd7bb46
Denis Doria <denisdoria@gmail.com>
2021-11-20 14:30:13 -0600

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# upower
+# supower
+
 Upower module for waybar
+
+# Waybar Configuration
+
+```
+"custom/mouse": {
+     "format": "{} {percentage}% {icon}",
+     "return-type": "json",
+     "exec": "$HOME/.config/waybar/scripts/supower.py --model 'MX Master 2S' --text '{Type}'",
+     "interval": 5,
+     "format-icons": ["", "", "", "", ""]
+    }
+```
+
+# Helper
+
+Lists devices showing their path and model.  
+Both can be used as arguments for --device/--model.  
+The preferable way is to use the mode.
+
+```
+./supower.py --list-devices
+/org/freedesktop/UPower/devices/line_power_AC
+/org/freedesktop/UPower/devices/battery_BAT0    02DL007
+/org/freedesktop/UPower/devices/mouse_dev_C3_BB_BF_80_6D_61     MX Master 2S
+/org/freedesktop/UPower/devices/headset_dev_04_5D_4B_40_CA_6D   MDR-1000X
+```
---
+++ supower.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+import dbus
+import click
+import json
+import sys
+
+
+PROPERTIES = {
+    'BatteryLevel': ('unknown', 'none', 'low', 'critical', 'normal', 'high', 'full'),
+    'Capacity': None,
+    'Energy': None,
+    'EnergyEmpty': None,
+    'EnergyFull': None,
+    'EnergyFullDesign': None,
+    'EnergyRatev': None,
+    'HasHistory': None,
+    'HasStatistics': None,
+    'IconName': None,
+    'IsPresent': None,
+    'IsRechargeable': None,
+    'Luminosity': None,
+    'Model': None,
+    'NativePath': None,
+    'Online': None,
+    'Percentage': None,
+    'PowerSupply': None,
+    'Serial': None,
+    'State': ('unknown', 'charging', 'discharging', 'empty', 'fully charged', 'pending charge', 'pending discharge'),
+    'Technology': ('unknown', 'lithium ion', 'lithium polymer', 'lithium iron phosphate', 'lead acid', 'nickel cadmium', 'nickel metal hydride'),
+    'Temperature': None,
+    'TimeToEmpty': None,
+    'TimeToFull': None,
+    'Type': ('unknown', 'line-power', 'battery', 'ups', 'monitor', 'mouse', 'keyboard', 'pda', 'phone', 'media-player',
+            'tablet', 'computer', 'gaming_input', 'pen', 'touchpad', 'modem', 'network', 'headset', 'speakers',
+            'headphones', 'video', 'other_audio', 'remote_control', 'printer', 'scanner', 'camera', 'wearable',
+            'toy', 'bluetooth-generic'),
+    'UpdateTime': None,
+    'Vendor': None,
+    'Voltage': None,
+    'WarningLevel': ('unknown', 'none', 'discharging', 'low', 'critical', 'action')
+}
+
+
+def device_info(bus, device):
+    result = {'all': []}
+    device_proxy = bus.get_object('org.freedesktop.UPower', device)
+    device_interface = dbus.Interface(device_proxy, 'org.freedesktop.DBus.Properties')
+    for _property, friendly_name in PROPERTIES.items():
+        try:
+            data = device_interface.Get('org.freedesktop.UPower.Device', _property)
+            result[_property] = friendly_name[data] if friendly_name else data
+        except dbus.exceptions.DBusException:
+            pass
+
+    return result
+
+def get_devices(bus):
+    devices_proxy = bus.get_object('org.freedesktop.UPower', '/org/freedesktop/UPower')
+    devices_interface = dbus.Interface(devices_proxy, 'org.freedesktop.UPower')
+    devices = devices_interface.EnumerateDevices()
+
+    return devices
+
+def get_device(bus, devices, device):
+    if device in devices:
+        return device
+
+    for path in devices:
+        inspect = device_info(bus, path)
+        if inspect.get('Model') == device:
+            return path
+
+    raise Exception("Device Not Found")
+
+def output_devices(bus, devices):
+    for device in devices:
+        print(f'{device}\t{device_info(bus, device).get("Model")}')
+    sys.exit(0)
+
+@click.command()
+@click.option('--list-devices', is_flag=True, help='List devices and models')
+@click.option('--device', '--model', help='Path or Model')
+@click.option('--text', show_default = True, default="{Model}")
+@click.option('--alt', show_default = True, default="{BatteryLevel}")
+@click.option('--tooltip', show_default = True, default="{Type}\n{NativePath}")
+@click.option('--class', '_class', show_default = True, default="{BatteryLevel}")
+@click.option('--percentage', show_default = True, default="{Percentage:.0f}")
+def main(list_devices, device, text, alt, tooltip, _class, percentage):
+    """
+    TEXT can be replaced using one or more {KEY}\n
+    {BatteryLevel} {Capacity} {Energy} {EnergyEmpty} {EnergyFull}
+    {EnergyFullDesign} {EnergyRatev} {HasHistory} {HasStatistics}
+    {IconName} {IsPresent} {IsRechargeable} {Luminosity}
+    {Model} {NativePath} {Online} {Percentage} {PowerSupply}
+    {Serial} {State} {Technology} {Temperature} {TimeToEmpty}
+    {TimeToFull} {Type} {UpdateTime} {Vendor} {Voltage}
+    {WarningLevel}
+
+    Example: upower.py --model 'MX Master 2S' --tooltip '{State}'
+
+    """
+    bus = dbus.SystemBus()
+    devices = get_devices(bus)
+
+    if list_devices:
+        output_devices(bus, devices)
+
+
+    try:
+        device = get_device(bus, devices, device)
+        info = device_info(bus, device)
+        output = {
+            "text": text.format(**info),
+            "alt": alt.format(**info),
+            "tooltip": tooltip.format(**info),
+            "class": _class.format(**info),
+            "percentage": float(percentage.format(**info))
+        }
+    except Exception as e:
+        output = {"text": f'{device.split("/")[-1]} cannot be found'}
+
+    print(json.dumps(output))
+
+
+if __name__ == "__main__":
+    main()
